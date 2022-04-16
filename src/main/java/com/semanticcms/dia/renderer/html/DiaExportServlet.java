d31629bc1dbbe8a2810b90ce6e8154e9ad0b192d
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:01:44 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/dia/renderer/html/DiaExportServlet.java
+++ src/main/java/com/semanticcms/dia/renderer/html/DiaExportServlet.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-dia-renderer-html - Dia-based diagrams embedded in HTML in a Servlet environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-dia-renderer-html.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.dia.renderer.html;
 
 import com.aoapps.lang.io.ContentType;
--- src/main/java/com/semanticcms/dia/renderer/html/DiaHtmlRenderer.java
+++ src/main/java/com/semanticcms/dia/renderer/html/DiaHtmlRenderer.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-dia-renderer-html.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.dia.renderer.html;
 
 import com.aoapps.concurrent.KeyedConcurrencyReducer;
